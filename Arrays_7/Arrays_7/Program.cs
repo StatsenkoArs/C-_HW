e76ffd9b7747aa91aa0457c980f28bb15f171ac4
Arseniy <33318310+StatsenkoArs@users.noreply.github.com>
2023-11-03 11:32:20 +0300

    end of Arrays

---
+++ Arrays_7/Arrays_7/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] n = new int[N]; 
+string s = Console.ReadLine();
+string[] S = s.Split(' ');
+for (int i = 0; i < S.Length; i++)
+{
+    n[i] = int.Parse(S[i]);
+}
+for (int i = 2; i < N; i++)
+{
+    if ((n[i] == 0)) n[i] += n[i - 1] + n[i - 2];
+}
+Array.ForEach(n, x => Console.Write(x + " "));
